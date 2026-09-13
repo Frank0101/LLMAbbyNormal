d17de4de2ba07b5f2d9e30fc952283cf47f2039a
Francesco Cossu <francesco.cossu@gmail.com>
2026-09-13 18:57:21 +0100

    Added 1st version of the neuron

--- src/LLMAbbyNormal.ConsoleApp/Program.cs
+++ src/LLMAbbyNormal.ConsoleApp/Program.cs
@@ -1,1 +1,15 @@
-Console.WriteLine("hello world");
+using LLMAbbyNormal.Domain.Models.NeuralNetwork;
+
+Console.WriteLine("LLM Abby Normal");
+
+var neuron = new Neuron(3);
+neuron.Output.ValueEmitted += value
+    => Console.WriteLine($"Neuron has emitted value: {value}");
+
+
+for (var n = 0; n < 3; n++)
+{
+    var value = n + 1;
+    Console.WriteLine($"Neuron input #{n} receiving value: {value}");
+    neuron.Inputs[n].ReceiveValue(value);
+}
--- src/LLMAbbyNormal.Domain/Models/NeuralNetwork/Neuron.cs
+++ src/LLMAbbyNormal.Domain/Models/NeuralNetwork/Neuron.cs
@@ -2,7 +2,28 @@ namespace LLMAbbyNormal.Domain.Models.NeuralNetwork;
 
 public class Neuron
 {
-    public Neuron()
+    public Input[] Inputs { get; }
+    public Output Output { get; } = new();
+
+    public Neuron(int inputsCount)
     {
+        // Creates the inputs
+        Inputs = new Input[inputsCount];
+        for (var n = 0; n < Inputs.Length; n++)
+        {
+            Inputs[n] = new Input();
+
+            // Wires the event
+            Inputs[n].ValueReceived += input_received;
+        }
+    }
+
+    private void input_received()
+    {
+        // Check that all inputs have a value
+        if (!Inputs.All(i => i.HasValue)) return;
+
+        // Emit the output (sum of inputs for now)
+        Output.EmitValue(Inputs.Sum(i => i.ConsumeValue()));
     }
 }
